cade2578c83a13528963eaa25eccb8c4a88575ff
abhichaudhri <35929391+abhichaudhri@users.noreply.github.com>
2018-03-12 21:21:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,8 @@
-console.log('Loaded!');
-var img= document.getElementById("madi");
-var marginLeft=0;
-function marginRight () {
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+"px";
-}
-img.onclick = function() {
-    var interval = setInterval(marginRight,50);
+var button=document.getElementById("counter");
+var counter=0;
+button.onclick function(){
+    
+    counter=counter+1;
+    var span =document.getelementbyId("count");
+    span.innerHTML=counter.toString();
 }
